11071c1ff50e4284adabcce6b266293e2b7d5715
sekiguchi-daisuke <dream.theater.sd@gmail.com>
2025-07-06 14:57:05 +0900

    初回コミット

---
+++ sample/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/emp/start")
+    public String showSearch() {
+        return "employee/search";
+    }
+}
