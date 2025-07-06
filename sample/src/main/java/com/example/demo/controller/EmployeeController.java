c06d6a03c6be82d45a40c734b0c93e0c64e3377a
sekiguchi-daisuke <58385712+sekiguchi-daisuke@users.noreply.github.com>
2025-07-06 17:06:22 +0900

    コンストラクタインジェクションの追加

--- sample/src/main/java/com/example/demo/controller/EmployeeController.java
+++ sample/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EmployeeController {
 
+    private final Service service;
+    
     @GetMapping("/")
     public String index() {
         return "index";
